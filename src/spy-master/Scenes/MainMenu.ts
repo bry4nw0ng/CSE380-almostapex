dc1d4dab8396e7a1588e842d8c769e435bf7d690
bswitzer7963 <theberzerker@icloud.com>
2026-04-13 22:06:53 -0400

    merged

--- src/spy-master/Scenes/MainMenu.ts
+++ src/spy-master/Scenes/MainMenu.ts
@@ -131,11 +131,7 @@ export default class MainMenu extends SMScene {
         // Player
         this.player = this.add.animatedSprite(PlayerActor, "player1", "player");
         this.player.position.set(500, 600);
-<<<<<<< HEAD
-        this.player.scale.set(1.25, 1.25);
-=======
         this.player.scale.set(2, 2);
->>>>>>> braden
         this.player.addPhysics(new AABB(Vec2.ZERO, new Vec2(8, 8)), Vec2.ZERO, true, false);
         this.player.addAI(PlayerController);
         this.player.animation.play("IDLE");
