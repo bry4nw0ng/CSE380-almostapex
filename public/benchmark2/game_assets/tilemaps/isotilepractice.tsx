6f15d839d8b9d74bbb801b8223ba366abcff5572
bry4nw0ng <bwong32104@gmail.com>
2026-04-14 10:29:29 -0400

    BENCHMARK2

--- public/benchmark2/game_assets/tilemaps/isotilepractice.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="isotilepractice" tilewidth="64" tileheight="32" tilecount="1" columns="1">
- <image source="city-tileset-completed.png" width="64" height="32"/>
-</tileset>
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ import { AbilityEvent } from "./spy-master/Events";
             {name: AAControls.ABILITY1, keys: ["1"]},
             {name: AAControls.ABILITY2, keys: ["2"]},
             {name: AAControls.ABILITY3, keys: ["3"]},
-            {name: AAControls.PICKUP_ITEM, keys: ["q"]},
+            {name: AAControls.PICKUP_ITEM, keys: ["e"]},
             {name: AAControls.INTERACT, keys: ["e"]},
         ],
         useWebGL: false,                        // Tell the game we want to use webgl
