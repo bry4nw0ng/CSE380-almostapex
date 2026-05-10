c70299068ee20f919b8e4a775e12adf2c914d00c
bry4nw0ng <bwong32104@gmail.com>
2026-05-10 19:52:57 -0400

    structure

--- src/spy-master/Scenes/SMScene.ts
+++ src/spy-master/Scenes/SMScene.ts
@@ -1,11 +1,18 @@
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import Scene from "../../Wolfie2D/Scene/Scene";
-import OrthogonalTilemap from "../../Wolfie2D/Nodes/Tilemaps/OrthogonalTilemap";
-import LaserGun from "../GameSystems/ItemSystem/Items/LaserGun";
-import Healthpack from "../GameSystems/ItemSystem/Items/Healthpack";
 import Battler from "../GameSystems/BattleSystem/Battler";
 import IsometricTilemap from "../../Wolfie2D/Nodes/Tilemaps/IsometricTilemap";
 import Navmesh from "../../Wolfie2D/Pathfinding/Navmesh";
+import Item from "../GameSystems/ItemSystem/Item";
+import {
+    BossDef,
+    EndLevelSpriteDef,
+    EnemyDef,
+    ItemKey,
+    LayerDepthMap,
+    SceneCtor,
+    WaveDef,
+} from "./LevelTypes";
 
 
 export default abstract class SMScene extends Scene {
@@ -17,4 +24,36 @@ export default abstract class SMScene extends Scene {
     public abstract isTargetVisible(position: Vec2, target: Vec2): boolean;
 
     public abstract getNavmesh(): Navmesh;
+
+    public abstract getLevelKey(): string;
+
+    public abstract getTilemapKey(): string;
+
+    public abstract getTilemapPath(): string;
+
+    public abstract getSpawnPosition(): Vec2;
+
+    public abstract getLayerDepthMap(): LayerDepthMap;
+
+    public abstract getMusicKey(): string;
+
+    public abstract getMusicPath(): string;
+
+    public abstract getEnemyTypes(): EnemyDef[];
+
+    public abstract getBoss(): BossDef | null;
+
+    public abstract getWaveConfig(): WaveDef[];
+
+    public abstract getShopInventory(): Item[];
+
+    public abstract getDropItemPool(): ItemKey[];
+
+    public abstract getEndLevelLocation(): Vec2;
+
+    public abstract getEndLevelLabel(): string;
+
+    public abstract getEndLevelSprite(): EndLevelSpriteDef;
+
+    public abstract getNextLevel(): SceneCtor | null;
 }
