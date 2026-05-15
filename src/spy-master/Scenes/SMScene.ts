6a8ee471676d730413fa5d258febbee32b6038b5
bswitzer7963 <theberzerker@icloud.com>
2026-05-15 08:44:28 -0400

    Merged

--- src/spy-master/Scenes/SMScene.ts
+++ src/spy-master/Scenes/SMScene.ts
@@ -380,7 +380,7 @@ export default abstract class SMScene extends Scene {
         this.battlers.forEach((battler) => {
             if (battler instanceof NPCActor) {
                 if (battler.position.distanceTo(needlePosition) < 70) {
-                    battler.health = battler.health - 0.1 + (this.player.damageIncrease - 1);
+                    battler.health = battler.health - 0.1 * (this.player.damageIncrease);
                 }
             }
         });
@@ -554,6 +554,7 @@ export default abstract class SMScene extends Scene {
                 break;
             case 8:
                 sprite = this.add.sprite("Coral", "equippables");
+                sprite.scale.set(0.75, 0.75);
                 newOb = new Coral(sprite);
                 break;
             case 9:
